5888c9574610a118c0007162839848ae68e8ea59
yosef_hershkovitz <4195228@gmail.com>
2023-05-02 16:46:58 +0300

    refactoring utils.c .h

--- Header_Files/utils.h
+++ Header_Files/utils.h
@@ -1,6 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX_LEN 1024
 
-int add_user();
+
+#endif
--- Source_Files/utils.c
+++ Source_Files/utils.c
@@ -1,12 +1,1 @@
 #include "../Header_Files/utils.h"
-int add_user()
-{
-    int buffer[21] = {0};
-    puts("please insert user name - up to 20 char");
-    fgets(buffer, sizeof(buffer), stdin);
-    //////// adding to a struct
-    puts("please insert password - up to 20 char");
-    fgets(buffer, sizeof(buffer), stdin);
-    //////// adding to a struct
-    return 0;
-}
